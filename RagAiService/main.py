ce62f8a84733ada64879aeb124dd4be4d60505a0
2221050451-NghiemDucMinh <minh2632004@gmail.com>
2026-05-01 23:27:19 +0700

    Vector Database (Cơ sở dữ liệu Vector)

--- RagAiService/main.py
+++ RagAiService/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import pdfplumber
+import chromadb
+import os
 import io
 
 #Khởi tạo ứng dụng FastAPI
@@ -19,6 +22,51 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# ==========================================
+# KHỞI TẠO VECTOR DATABASE (CHROMADB)
+# ==========================================
+
+DB_PATH = "./data/chroma_db_data"
+try:
+    chroma_client = chromadb.PersistentClient(path=DB_PATH)
+    print(f"Đã kết nốt thành công đến ChromaDB tại: {DB_PATH}")
+except Exception as e:
+    print(f"Lỗi khi khởi tạo ChromaBD: {e}")
+
+#Tạo 1 collection trong ChromaDB (tương tạo 1 bảng trong SQL)
+collection = chroma_client.get_or_create_collection(
+    name="enterprise_knowledge",
+    metadata={"hnsw:space":"cosine"}
+)
+
+# ==========================================
+# CÁC HÀM HỖ TRỢ (HELPER FUNCTIONS)
+# ==========================================
+def extract_text_from_bytes(file_bytes: bytes) -> str:
+    """Hàm hỗ trợ: Đọc file PDF từ byte và trả về chuỗi văn bản"""
+    text_content= []
+    with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text_content.append(page_text)
+
+    return "\n".join(text_content)
+
+def chunk_text(text: str) ->list[str]:
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap =200,
+        length_function=len,
+        separators=['\n\n','\n','.',' ','']
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# ==========================================
+# CÁC API ENDPOINTS
+# ==========================================
+
 @app.get("/")
 def read_root():
     return{
@@ -27,40 +75,31 @@ def read_root():
         "version": "1.0.0"
     }
 
-@app.post("/api/extract-text")
-async def extract_text_from_pdf(file: UploadFile = File(...)):
-    #1. Kiểm tra định dạng file
+@app.post("/api/ingest-test")
+async def test_ingestion_pipeline(file: UploadFile = File(...)):
     if not file.filename.endswith(".pdf"):
-        raise HTTPException(status_code=400, detail="Chỉ hỗ trợ định dạng file PDF");
-
-    text_content =""
+        raise HTTPException(status_code=400, detail="Chỉ hỗ trợ định dạng PDF")
 
     try:
-        #2. Đọc file từ bộ nhớ tạm thành luồng byte
         file_bytes = await file.read()
 
-        #3.Mở file pdf bằng pdfplumber
-        with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
-            for page in pdf.pages:
-                page_text = page.extract_text()
+        raw_text = extract_text_from_bytes(file_bytes)
 
-                if page_text:
-                    text_content += page_text + "\n"
+        if not raw_text.strip():
+            return {"status": "warning", "message": "Không tìm thấy chữ trong PDF."}
         
-        #4.Kiểm tra xem file có bị rỗng hay file chỉ chứa ảnh 
-        if not text_content.strip():
-            return {
-                "status":"warning",
-                "message":"Không tìm thấy chữ trong file PDF (có thể là ảnh chụp)",
-                "text":""
-            }
-        
+        document_chunks = chunk_text(raw_text)
+
         return{
-            "status": "success",
+            "status":"success",
             "filename":file.filename,
-            "total_pages":len(pdf.pages),
-            "text_review": text_content[:200] +"..."
+            "total_characters":len(raw_text),
+            "total_chunk":len(document_chunks),
+            "sample_chunk_1": document_chunks[0] if len(document_chunks)>0 else "",
+            "sample_chunk_2": document_chunks[1] if len(document_chunks)>1 else ""
         }
+    
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Lỗi khi xử lý file PDF: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Lỗi khi xử lý: {str(e)}")
+
 
